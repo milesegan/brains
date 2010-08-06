3112a5c986dd70ac846e99b9878df59f5bc17db4
Miles Egan <milesegan@gmail.com>
2010-08-06 10:29:13 -0700

    hide private kmeans funcs

--- clustering/kmeans.clj
+++ clustering/kmeans.clj
@@ -3,32 +3,32 @@
 (ns clustering
   (require [clojure.string :as string]))
 
-(defn read-file [path]
+(defn- read-file [path]
   (let [lines (line-seq (-> path (java.io.FileReader.) (java.io.BufferedReader.)))
         lines (map #(string/split % #", ") (rest lines))]
     (for [line lines]
       {:label (first line)
-       :values (map #(Double. %) (rest line))})))
+       :values (map #(Double. ^String %) (rest line))})))
 
-(defn centroid [points]
+(defn- centroid [points]
   (let [vals (map :values points)
         n (count (first vals))
         sums (for [i (range n)]
                (apply + (map #(nth % i) vals)))]
     (map #(/ % (count points)) sums)))
 
-(defn distance [a b]
+(defn- distance [a b]
   (let [squares (map (fn [x y] (Math/pow (- x y) 2))
                      a b)]
     (Math/sqrt (apply + squares))))
 
-(defn closest-centroid [p centroids]
+(defn- closest-centroid [p centroids]
   (let [distances (map-indexed (fn [i c]
                                  [(distance c (:values p)) i])
                                centroids)]
     (nth (first (sort distances)) 1)))
 
-(defn initial-centroids [k points]
+(defn- initial-centroids [k points]
   (map :values (take k (shuffle points))))
 
 (defn cluster [k points]
