6450a74b9daa7ddfb69220a6ebc32a159f2ef37d
Miles Egan <milesegan@gmail.com>
2010-08-08 07:55:20 -0700

    cleanup clj cluster funcs

--- clj/src/brains/clustering/single_link.clj
+++ clj/src/brains/clustering/single_link.clj
@@ -4,23 +4,23 @@
 
 (def mdistance (memoize distance))
 
-(defn build-clusters [d points]
-  (loop [points points
-         clusters nil]
-    (if (empty? points)
-      clusters
-      (let [p (first points)
-            others (rest points)
-            parts (seq/separate #(> d (mdistance (:values p) (:values %)))
-                                others)
-            [close far] parts]
-        (recur far (cons (cons p close) clusters))))))
+(defn build-clusters [d points clusters]
+  (if (empty? points)
+    clusters
+    (let [p (first points)
+          others (rest points)
+          is-close? #(> d (mdistance
+                           (:values p) (:values %)))
+          parts (seq/separate is-close? others)
+          [close far] parts]
+      (recur d far (cons (cons p close)
+                         clusters)))))
 
 (defn cluster [k points]
   (loop [clusters points
          distance 0]
     (let [oldcount (count clusters)
-          newclusters (build-clusters distance points)
+          newclusters (build-clusters distance points nil)
           newcount (count newclusters)]
       (if (= newcount k)
         newclusters
