1f183af477dd8037fb7e047c800f27c44191bfd2
Miles Egan <milesegan@gmail.com>
2010-08-17 10:49:45 -0700

    add package object for scaladoc

---
+++ src/main/scala/brains/clustering/SelfOrganizingMap.scala
@@ -0,0 +1,13 @@
+package brains.clustering
+
+import brains.NumericDataPoint
+
+/**
+ * Clusters numeric datasets via the self organizing map method.
+ * @see <a href="http://en.wikipedia.org/wiki/Self_organizing_map">self organizing map</a>.
+ */
+abstract class SelfOrganizingMap extends Method {
+  
+}
+
+object SelfOrganizingMap extends Driver
---
+++ src/main/scala/brains/package.scala
@@ -0,0 +1,8 @@
+/**
+ * Brains is a library of machine learning algorithms.
+ *
+ * It includes tools for supervised and unsupervised learning
+ * and recommendation systems.
+ */
+package object brains
+
