3cd35f1a21d0cdec54699c038af13d608d63ae9e
Miles Egan <milesegan@gmail.com>
2010-09-29 21:27:53 -0700

    comment map +? operator

--- src/main/scala/brains/Util.scala
+++ src/main/scala/brains/Util.scala
@@ -3,9 +3,15 @@ package brains
 import collection.mutable.{ Map => MMap }
 
 object util {
+
+  /**
+   * Adds +? shorthand operator for getOrElseUpdate to mutable
+   * maps.
+   */
   class MutableMapGetOrElseUpdateWrapper[A,B](m: MMap[A,B]) {
     def +?(k:A, v:B) = m.getOrElseUpdate(k, v)
   }
 
   implicit def map2MutWrapper[A,B](m:MMap[A,B]) = new MutableMapGetOrElseUpdateWrapper(m)
+
 }
