15ab9c7830d77328073dabc08fcded3ae31e6b9d
Miles Egan <milesegan@gmail.com>
2010-11-07 12:00:51 +0100

    neural net working on iris dataset, needs tuning for others

--- brains/neural_net.py
+++ brains/neural_net.py
@@ -1,53 +1,51 @@
 from __future__ import division
 import math
 import numpy as np
-from random import random
 
 class NeuralNet(object):
     """Basic single-layer neural net implementation."""
     def __init__(s, n_inputs, n_outputs, n_hidden):
+        assert n_inputs > 0 and n_outputs > 0 and n_hidden > 0
         s.eta = 0.1 # learning rate
         s.n_inputs = n_inputs
         s.n_outputs = n_outputs
         s.n_hidden = n_hidden
-        s.iweights = s.__random_weights(n_hidden, n_inputs + 1) # add one for bias node
-        s.oweights = s.__random_weights(n_outputs, n_hidden + 1)
+        # initialize weights to small random number
+        s.iweights = (np.random.rand(n_inputs, n_hidden) - 0.5) * 2 / math.sqrt(n_inputs)
+        s.oweights = (np.random.rand(n_hidden, n_outputs) - 0.5) * 2 / math.sqrt(n_hidden)
 
     def train(s, inputs, outputs):
-        hact, oact = s.process(inputs)
-        # find output error
-        oerr = [(outputs[i] - oact[i]) * oact[i] * (1 - oact[i]) for i in range(s.n_outputs)]
-        oerr = (outputs - oact) * oact
-    #     # find hidden layer error
-    # // calc hidden layer error
-    # val hErr = for (j <- 0 until nHidden) yield {
-    #   val errProd = for (k <- 0 until nOutputs) yield oWeights(j)(k) * oErr(k)
-    #   hAct(j) * (1 - hAct(j)) * errProd.sum
-    # }
-        print(oerr)
-        for i in range(s.n_outputs):
-            for j in range(s.n_hidden):
-                s.oweights[i][j] += s.eta * oerr[i] * hact[j]
-        # for i in range(s.n_inputs):
-        #     for j in range(s.n_hidden):
-        #         s.iweights[i][j] += s.eta * herr[i] * inputs[j]
+        """Trains the network with the given sample."""
+        assert inputs.shape[1] == s.n_inputs
+        assert outputs.shape[1] == s.n_outputs
+        iterations = 0
+        order = range(inputs.shape[0])
+        while iterations < 2000:
+            # find activations and error
+            hact, oact = s.process(inputs)
+            error = 0.5 * ((outputs - oact) ** 2).sum() / len(inputs)
+            if error < 0.005: break
+            oerr = (outputs - oact) * oact * (1.0 - oact)
+            herr = hact * (1 - hact) * oerr.dot(s.oweights.T)
+
+            # update weights
+            updatei = s.eta * inputs.T.dot(herr)
+            updateo = s.eta * hact.T.dot(oerr)
+            s.oweights += updateo
+            s.iweights += updatei
+
+            # shuffle order of weights & inputs to speed learning
+            np.random.shuffle(order)
+            inputs = inputs[order,:]
+            outputs = outputs[order,:]
+            iterations += 1
 
     def process(s, inputs):
         """Runs the neural net on the given inputs."""
-        assert len(inputs) == s.n_inputs
-        hact = [s.__activation(v) for v in np.dot(s.iweights, np.append(inputs, -1))]
-        oact = [s.__activation(v) for v in np.dot(s.oweights, np.append(hact, -1))]
+        assert inputs.shape[1] == s.n_inputs
+        hact = inputs.dot(s.iweights)
+        hact = 1 / (1 + np.exp(-hact))
+        oact = hact.dot(s.oweights)
+        oact = 1 / (1 + np.exp(-oact))
         return hact, oact
-        
-    def __activation(s, val):
-        """Activation function. Default to sigmoid."""
-        return 1 / (1 + math.exp(val))
-
-    def __random_weights(s, rows, columns):
-        """Initializes weights to a small, non-zero value."""
-        max_init = 1.0 / math.sqrt(s.n_hidden)
-        w = []
-        for r in range(rows):
-            w.append([random() * 2 * max_init - max_init for i in range(columns)])
-        return w
     
--- tests/test_neural_net.py
+++ tests/test_neural_net.py
@@ -5,14 +5,22 @@ import numpy as np
 
 class TestNeuralNet(unittest.TestCase):
     
-    def test_create(s):
-        n = NeuralNet(3, 1, 4)
-        s.assertEqual(np.shape(n.iweights), (4, 4))
-        s.assertEqual(np.shape(n.oweights), (1, 5))
-        
     def test_train(s):
-        d = np.loadtxt("data/wine.csv", skiprows = 2, delimiter = ",")
+        d = np.loadtxt("data/iris.csv", skiprows = 6, delimiter = ",", comments = "#")
+        np.random.shuffle(d)
+        targets, inputs = d[:,0], d[:,1:]
+        targets.shape = (targets.shape[0], 1)
+        targets /= targets.max()
+        inputs -= inputs.mean(axis = 0)
+        split = d.shape[0] / 4
+        testdata, traindata = inputs[:split,:], inputs[split:,:]
+        order = range(traindata.shape[0])
+        testtargets, traintargets = targets[:split,:], targets[split:,:]
         n = NeuralNet(np.shape(d)[1] - 1, 1, 4)
-        for i in range(10):
-            for p in d:
-                n.train(p[1:], [p[0]])
+        n.train(traindata, traintargets)
+        hact, p = n.process(testdata)
+        for i in range(len(p)):
+            continue
+            print testtargets[i], p[i]
+
+
